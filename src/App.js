70698b334a779e0de8541bde6b85fae240eee101
YesidBarragan <yesarmand2006@hotmail.com>
2021-09-07 21:33:23 -0500

    [ADD] Components files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Historia from "./components/Historia/Historia";
+import Opciones from "./components/Opciones/Opciones";
+import Recordatorio from "./components/Recordatorio/Recordatorio";
+
 function App() {
   return (
     <div className="App">
+      <Historia/>
+      <Opciones/>
+      <Recordatorio/>
     </div>
   );
 }
---
+++ src/components/Opciones/Opciones.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Opciones extends Component {
+  render() {
+    return (
+      <></>
+    );
+  }
+}
